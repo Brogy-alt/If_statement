8287922e381078172f56383d5fd0bcbd2964b8ae
Brogy-alt <brogangys2@gmail.com>
2022-11-11 10:18:04 +0200

    got it 70%

---
+++ js/code.js
@@ -0,0 +1,78 @@
+
+
+// if statment
+// let numb = 12;
+// if(numb > 13) {
+//     document.write ("Great than 13");
+// }else {
+//     document.write("less than 13");
+// }
+
+// Using a function
+
+// function isNumber (onlyNumber) {
+//     if(Number.isFinite(onlyNumber)) {
+//         document.write (`${onlyNumber}is a number`);
+//     }else {
+//         document.write(`${onlyNumber}is not a number`);
+            
+//         }  
+// }
+
+// calling function
+// let myNumber = 7.6;
+// isNumber(myNumber);
+
+// challenge
+
+// let numb = 90;
+// let myNumber = 105 ;
+
+let onlyAge = 18;
+let myAge = 21;
+
+
+// if (myNumber > numb) {
+//     document.write ("Great than 100");
+// }else {
+//     document.write ("less than 100");
+// }
+
+if (myAge > onlyAge) {
+    document.write ("adult");
+}else {
+    document.write ("Not an adult");
+}
+
+// if(numb >= 100) {
+//     document.write ("Great or equals than 100");
+// }else {
+//     document.write ("Less than 100");
+
+// }
+
+function addition (onlyNumber , onlyAge ) {
+    if(Number.isFinite(onlyNumber)) {
+        document.write (`${onlyNumber}is a number`);
+        
+    }else {
+        document.write(`${onlyNumber}is not a number`);
+        
+    }if(Words.isFinite(onlyAge)) {
+        document.write (`${onlyAge} is a young`)
+
+    }else {
+        document.write(`${onlyAge} is not young`)
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
